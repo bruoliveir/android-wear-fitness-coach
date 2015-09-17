62ab7cc59efc102bb8a420e27bf59425ea30aea5
Bruno Oliveira <bru.oliveir@gmail.com>
2015-09-16 23:41:46 -0300

    wear layout elements; countdown

--- wear/src/main/java/com/example/weartest/MainActivity.java
+++ wear/src/main/java/com/example/weartest/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.weartest;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.text.method.CharacterPickerDialog;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends WearableActivity {
 
-    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
-            new SimpleDateFormat("HH:mm", Locale.US);
-
     private BoxInsetLayout mContainerView;
-    private TextView mTextView;
-    private TextView mClockView;
+    private TextView mTextViewClock;
+    private TextView mTextViewSets;
+    private Button mButtonComplete;
+
+    private CountDownTimer mCountDownTimer;
+    private int mCurrentSet = 0;
+    private int mTotalSets = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,41 @@ public class MainActivity extends WearableActivity {
         setAmbientEnabled();
 
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        mClockView = (TextView) findViewById(R.id.clock);
+        mTextViewClock = (TextView) findViewById(R.id.clock);
+        mTextViewSets = (TextView) findViewById(R.id.sets);
+        mButtonComplete = (Button) findViewById(R.id.complete);
+
+        mButtonComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mCurrentSet++;
+                if (mCurrentSet >= 10) mTextViewClock.setText("ALL DONE!");
+                mTextViewSets.setText(mCurrentSet + "/" + mTotalSets);
+
+                long start = 10000;
+                long step = 1000;
+
+                mCountDownTimer = new CountDownTimer(start + 1000, step) {
+                    @Override
+                    public void onTick(long l) {
+                        mTextViewClock.setText("" + (l / 1000));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        mTextViewClock.setText("GO GO GO");
+
+                        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                        long[] vibrationPattern = {0, 500, 50, 500, 50, 500};
+                        final int indexInPatternToRepeat = -1;
+                        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+                    }
+                };
+
+                mCountDownTimer.start();
+            }
+        });
     }
 
     @Override
@@ -51,14 +86,15 @@ public class MainActivity extends WearableActivity {
     private void updateDisplay() {
         if (isAmbient()) {
             mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
-            mTextView.setTextColor(getResources().getColor(android.R.color.white));
-            mClockView.setVisibility(View.VISIBLE);
+            mTextViewClock.setTextColor(getResources().getColor(android.R.color.white));
+            mTextViewSets.setTextColor(getResources().getColor(android.R.color.white));
 
-            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+            mTextViewClock.setText(getString(R.string.clock));
+            mTextViewSets.setText(getString(R.string.sets));
         } else {
-            mContainerView.setBackground(null);
-            mTextView.setTextColor(getResources().getColor(android.R.color.black));
-            mClockView.setVisibility(View.GONE);
+            mContainerView.setBackgroundColor(getResources().getColor(R.color.main_background));
+            mTextViewClock.setTextColor(getResources().getColor(android.R.color.primary_text_dark));
+            mTextViewSets.setTextColor(getResources().getColor(android.R.color.secondary_text_dark));
         }
     }
 }
