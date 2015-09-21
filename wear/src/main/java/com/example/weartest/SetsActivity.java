b9ec2a0b8867064cbd2d4906aba50117249a0266
Bruno Oliveira <bru.oliveir@gmail.com>
2015-09-21 12:28:11 -0300

    destroy SetsActivity after leaving

--- wear/src/main/java/com/example/weartest/SetsActivity.java
+++ wear/src/main/java/com/example/weartest/SetsActivity.java
@@ -1,6 +1,7 @@
 package com.example.weartest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
@@ -9,20 +10,24 @@ import android.support.wearable.view.WearableListView;
 public class SetsActivity extends Activity implements WearableListView.ClickListener {
 
     public static final String NUMBER_OF_SETS = "NUMBER_OF_SETS";
+    private static final String[] elements = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
-    String[] elements = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sets);
+
+        mContext = this;
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 WearableListView listView = (WearableListView) findViewById(R.id.wearable_list);
-                listView.setAdapter(new SetsAdapter(SetsActivity.this, elements));
-                listView.setClickListener(SetsActivity.this);
+                listView.setAdapter(new SetsAdapter(mContext, elements));
+                listView.setClickListener((WearableListView.ClickListener) mContext);
             }
         });
     }
@@ -34,6 +39,8 @@ public class SetsActivity extends Activity implements WearableListView.ClickList
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra(NUMBER_OF_SETS, elements[tag]);
         startActivity(intent);
+
+        finish();
     }
 
     @Override
